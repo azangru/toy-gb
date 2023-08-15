72861f91b0a241dfdb5ec43b6c95e1c17889d9e0
Andrey Azov <andrey@ebi.ac.uk>
2023-08-15 23:06:08 +0100

    Genes can be read from bigbed file

--- src/server/genes.ts
+++ src/server/genes.ts
@@ -1,7 +1,8 @@
 import  { BigBed } from '@gmod/bbi';
-import { RemoteFile } from 'generic-filehandle';
+import BED from '@gmod/bed';
+import { RemoteFile, LocalFile } from 'generic-filehandle';
 
-const filePath = 'https://ensembl-2020-gb-flatfiles.s3.eu-west-2.amazonaws.com/upload-20220829/genes_and_transcripts/homo_sapiens_GCA_000001405.28/transcripts.bb';
+// const filePath = 'https://ensembl-2020-gb-flatfiles.s3.eu-west-2.amazonaws.com/upload-20220829/genes_and_transcripts/homo_sapiens_GCA_000001405.28/transcripts.bb';
 // const filePath = 'https://ensembl-2020-gb-flatfiles.s3.eu-west-2.amazonaws.com/upload-20220829/regulation/homo_sapiens_GCA_000001405.28/regulatory-features.bb';
 
 
@@ -9,11 +10,23 @@ const filePath = 'https://ensembl-2020-gb-flatfiles.s3.eu-west-2.amazonaws.com/u
 
 const readGenes = async () => {
   try {
+    // const file = new BigBed({
+    //   filehandle: new RemoteFile(filePath, { fetch: (globalThis as any).fetch }),
+    // });
     const file = new BigBed({
-      filehandle: new RemoteFile(filePath, { fetch: (globalThis as any).fetch }),
+      filehandle: new LocalFile(require.resolve('/Users/andrey/development/experiments/toy-gb/data/transcripts.bb')),
     });
     const { autoSql } = await file.getHeader();
-    // console.log('autoSql', autoSql);
+
+    const features = await file.getFeatures('1', 0, 100000)
+    const bedParser = new BED({ autoSql });
+
+    const lines = features.map(feature => {
+      const { start, end, rest, uniqueId } = feature;
+      return bedParser.parseLine(`chr7\t${start}\t${end}\t${rest}`, { uniqueId })
+    });
+
+    console.log(JSON.stringify(lines, null, 2));
 
 
 
